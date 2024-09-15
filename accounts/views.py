92fddc030718ae41eb0309728a5aa0e3f63a6be0
AJMAL M T <ajmalmt67@gmail.com>
2024-09-15 22:07:03 +0530

    django authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
@@ -74,8 +75,9 @@ def login(request):
         # if we having user with such credential
         if user is not None:
             auth.login(request, user)
-            # messages.success(request, "You are now logged in")
-            return redirect('home')
+            messages.success(request, "You are now logged in")
+            # return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, "Invalid login credentials")
             return redirect('login')
@@ -102,8 +104,86 @@ def activate(request, uidb64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        messages.success(request, 'Congratualtions! Your account is activated')
+        messages.success(request, 'Congratulations! Your account is activated')
         return redirect('login')
     else:
         messages.error(request, 'invalid activation link')
         return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            # Here we __exact we checking that the email address user entered exactly same as what we have in database
+            user = Account.objects.get(email__exact=email)
+
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = "Reset You Password"
+            message = render_to_string(
+                'accounts/reset_password_email.html', {
+                    'user': user,
+                    'domain': current_site,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': default_token_generator.make_token(user)
+                })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(
+                request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    # if this kind of below error happens then seet user none
+    except (TypeError, ValueError, Account.DoesNotExist):
+        user = None
+
+    # if the user is not none check the user token
+    # purpose of checking token is to know this secure request or not
+    if user is not None and default_token_generator.check_token(user, token):
+        # Then next we want to save uid in session
+        # Beacuse later we need this uid for resetting password
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('login')
+
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            # take the uid from the session
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            # set_password is the inbuilt function of django
+            # what actually happen is,take the password and it saved in hashed format
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
